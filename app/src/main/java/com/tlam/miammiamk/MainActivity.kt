9c4114fb2d874f09ff4e9abb0e6950afce5a237e
Thierry <lamthierry@gmail.com>
2017-12-03 15:23:23 -0500

    Adding a few more images

--- app/src/main/java/com/tlam/miammiamk/MainActivity.kt
+++ app/src/main/java/com/tlam/miammiamk/MainActivity.kt
@@ -33,24 +33,24 @@ class MainActivity : AppCompatActivity() {
                 "Japanese",
                 "Asian",
                 listOf(
-                        Food("Makizushi", "Cylindrical piece", "https://cdn.pixabay.com/photo/2017/02/05/11/48/sushi-2039735__340.jpg"),
-                        Food("Nigirizushi", "Topping on oval shaped ball of rice", "https://cdn.pixabay.com/photo/2016/03/05/22/23/asian-1239272__340.jpg")))
+                        Food("Makizushi", "Cylindrical piece", "https://cdn.pixabay.com/photo/2016/03/05/22/23/asian-1239272__340.jpg"),
+                        Food("Nigirizushi", "Topping on oval shaped ball of rice", "https://cdn.pixabay.com/photo/2017/02/05/11/48/sushi-2039735__340.jpg")))
         cuisineList.add(cuisine)
 
         cuisine = Cuisine(
                 "Italian",
                 "European",
                 listOf(
-                        Food("Farfalle", "Butterfly looking", "nigirizushi"),
-                        Food("Spaghetti", "Long, thin and cylindrical", "nigirizushi")))
+                        Food("Farfalle", "Butterfly looking", "https://cdn.pixabay.com/photo/2014/10/20/22/04/farfalle-495747__340.jpg"),
+                        Food("Spaghetti", "Long, thin and cylindrical", "https://cdn.pixabay.com/photo/2017/11/08/22/18/spaghetti-2931846__340.jpg")))
         cuisineList.add(cuisine)
 
         cuisine = Cuisine(
                 "Mexican",
                 "Central America",
                 listOf(
-                        Food("Enchilada", "Corn tortilla rolled around a filling and covered with a chili pepper sauce", "nigirizushi"),
-                        Food("Fajita", "Any grilled meat usually served as a taco", "nigirizushi")))
+                        Food("Enchilada", "Corn tortilla rolled around a filling and covered with a chili pepper sauce", "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240__340.jpg"),
+                        Food("Fajita", "Any grilled meat usually served as a taco", "https://cdn.pixabay.com/photo/2014/11/07/17/14/tortillas-520808__340.jpg")))
         cuisineList.add(cuisine)
 
         adapter?.notifyDataSetChanged()
